6be2dae06a02c1ae0556e984b0dbd5472212afa9
Edgaras A <e.ausvicas@gmail.com>
2019-08-13 18:20:20 +0200

    Refactor Services

---
+++ Services/Discord/PublicDiscordCommandModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace StravaDiscordBot.Services.Discord
+{
+    public class PublicDiscordCommandModule : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
