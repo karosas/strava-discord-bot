16c2c4ad186c6072cb61aa7b9c573ec2c05866b2
Edgaras A <e.ausvicas@gmail.com>
2019-08-13 18:19:36 +0200

    Add extensions to add discord.net modules

---
+++ Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StravaDiscordBot.Extensions
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UseDiscordSocketCommandContextModule<T>(this IApplicationBuilder applicationBuilder) where T : ModuleBase<SocketCommandContext>
+        {
+            var commandService = applicationBuilder.ApplicationServices.GetService<CommandService>();
+            commandService.AddModulesAsync(Assembly.GetEntryAssembly(), applicationBuilder.ApplicationServices)
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
