99c77fb919cff4955ea31fe5f40afaed25d74302
Edgaras A <e.ausvicas@gmail.com>
2019-08-13 17:37:56 +0200

    Add initial project

---
+++ StravaDiscordBot/Discord/Modules/PublicModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace StravaDiscordBot.Discord.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
